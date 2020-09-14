de4ad523c45fc448b445e1783bdabb570110340e
Sam Mansfield <samgmansfield@gmail.com>
2020-09-13 23:00:42 -0700

    Added header docstrings and changed spacing from 4 to 2 to PIMAP sentinel components.

--- pimap/pimapanalyzeheatmap.py
+++ pimap/pimapanalyzeheatmap.py
@@ -1,37 +1,46 @@
+"""PIMAP Analyze component that analyzes Sentinel bandage data.
+
+PIMAP Analyze converts SentinelBandage PIMAP samples into SentinelHeatMapMetrics. This
+component depends on the sentineltoolkit.
+
+License:-
+Author: Eric Vin
+"""
 import pickle
 
 from pimap import pimaputilities as pu
 from sentineltoolkit.sentinel_analysis import analyze_sample_discrete_heatmap
 
 class PimapAnalyzeHeatmap:
-    """ Converts a calibrated sentinel bandage scan into a heatmap. """
-    def __init__(self, frequency, analysis_type):
-        # Which frequency to display on the heatmap
-        self.frequency = int(frequency)
-        # Which heatmap type: 0 for discrete, 1 for continuous
-        self.analysis_type = analysis_type
+  """ Converts a calibrated sentinel bandage scan into a heatmap. """
+  def __init__(self, frequency, analysis_type):
+    # Which frequency to display on the heatmap
+    self.frequency = int(frequency)
+    # Which heatmap type: 0 for discrete, 1 for continuous
+    self.analysis_type = analysis_type
 
-    def analyze(self, pimap_samples):
-        pimap_metrics = []
+  def analyze(self, pimap_samples):
+    pimap_metrics = []
 
-        for pimap_sample in pimap_samples:
-            pickled_scan = pu.get_data(pimap_sample).encode()
+    for pimap_sample in pimap_samples:
+      pickled_scan = pu.get_data(pimap_sample).encode()
 
-            sensor_data = pickle.loads(pickled_scan)
+      sensor_data = pickle.loads(pickled_scan)
 
-            # Discrete analysis shows each connection as its own block on the heatmap
-            if self.analysis_type == 0:
-                heatmap_data = analyze_sample_discrete_heatmap(sensor_data, self.frequency)
-            # Continuous analysis averages neigbouring connections, to produce a smoother heatmap
-            elif self.analysis_type == 1:
-                heatmap_data = analyze_sample_continuous_heatmap(sensor_data, self.frequency)
-            else:
-                assert False
+      # Discrete analysis shows each connection as its own block on the heatmap
+      if self.analysis_type == 0:
+          heatmap_data = analyze_sample_discrete_heatmap(sensor_data, self.frequency)
+      # Continuous analysis averages neigbouring connections, to produce a smoother
+      # heatmap
+      elif self.analysis_type == 1:
+          heatmap_data = analyze_sample_continuous_heatmap(sensor_data, self.frequency)
+      else:
+          assert False
 
-            pimap_metric = pickle.dumps(heatmap_data, 0).decode()
+      pimap_metric = pickle.dumps(heatmap_data, 0).decode()
 
-            pimap_metric = pu.create_pimap_metric("SentinelHeatmap", pimap_sample, pimap_metric)
+      pimap_metric = pu.create_pimap_metric("SentinelHeatmap", pimap_sample, pimap_metric)
 
-            pimap_metrics.append(pimap_metric)
+      pimap_metrics.append(pimap_metric)
 
-        return pimap_metrics
+      return pimap_metrics
--- pimap/pimapsensesentinel.py
+++ pimap/pimapsensesentinel.py
@@ -1,3 +1,11 @@
+"""PIMAP Sense component that interacts with the Sentinel bandage.
+
+PIMAP Sense Sentinel interacts with the Sentinel bandage using serial. This component
+depends on the sentineltoolkit.
+
+License:
+Author: Eric Vin
+"""
 import time
 import pickle
 
@@ -5,26 +13,30 @@ from pimap import pimaputilities as pu
 from sentineltoolkit.sentinel_interface import SentinelInterfaceToolkit
 
 class PimapSenseSentinel:
-    """ Performs a scan on the Sentinel Bandage """
-    def __init__(self, device_location, frequency_list, calibration_profile, patient_id = 0, device_id = 0):
-        # Creates an instance of the Sentinel Interface Toolkit
-        self.sentinel_toolkit = SentinelInterfaceToolkit(device_location)
+  """ Performs a scan on the Sentinel Bandage """
+  def __init__(self, device_location, frequency_list, calibration_profile,
+               patient_id = 0, device_id = 0):
+    # Creates an instance of the Sentinel Interface Toolkit
+    self.sentinel_toolkit = SentinelInterfaceToolkit(device_location)
 
-        # Stores list of frequencies to scan.
-        self.frequency_list = frequency_list
+    # Stores list of frequencies to scan.
+    self.frequency_list = frequency_list
 
-        # Stores calibration profile to calibrate raw scans
-        self.calibration_profile = calibration_profile
+    # Stores calibration profile to calibrate raw scans
+    self.calibration_profile = calibration_profile
 
-        self.patient_id = str(patient_id)
-        self.device_id = str(device_id)
+    self.patient_id = str(patient_id)
+    self.device_id = str(device_id)
 
     def sense(self):
-        raw_scan_results = self.sentinel_toolkit.send_scan_all(self.frequency_list)
-        calibrated_scan_results = self.calibration_profile.calibrate_raw_sample(raw_scan_results)
+      raw_scan_results = self.sentinel_toolkit.send_scan_all(self.frequency_list)
+      calibrated_scan_results = self.calibration_profile.calibrate_raw_sample(
+        raw_scan_results)
 
-        string_scan_results = pickle.dumps(calibrated_scan_results, 0).decode()
+      string_scan_results = pickle.dumps(calibrated_scan_results, 0).decode()
 
-        pimap_sample = pu.create_pimap_sample("SentinelBandage", self.patient_id, self.device_id, string_scan_results, time.time())
+      pimap_sample = pu.create_pimap_sample("SentinelBandage", self.patient_id,
+                                            self.device_id, string_scan_results,
+                                            time.time())
 
-        return [pimap_sample]
+      return [pimap_sample]
