170b644f08a6ce76dc9798c71b9858fb702582b7
Sam Mansfield <samgmansfield@gmail.com>
2020-11-12 10:58:15 -0800

    Added initial PIMAP Sense TCP implementation and associated tests.

---
+++ pimap/pimapsensetcp.py
@@ -0,0 +1,185 @@
+"""PIMAP Sense component that listens for TCP packets.
+
+PIMAP Sense TCP is a PIMAP Sense component that starts a multi-process server on a given
+host and port. PIMAP Sense TCP supports both IPv4 and IPv6.
+
+License:
+Author: Sam Mansfield
+"""
+import ast
+import ctypes
+import multiprocessing
+import socket
+import time
+from pimap import pimaputilities as pu
+
+class PimapSenseTcp:
+  def __init__(self, host="localhost", port=31415, sample_type="tcp", ipv6=False,
+               workers=3, system_samples=False):
+    """Constructor for PIMAP Sense TCP
+
+    Arguments:
+      host (optional): The host of the TCP server. Defaults to "localhost".
+      port (optional): The port of the TCP server. Defaults to 31415.
+      sample_type (optional): The sample type given to non-pimap sensed data.
+        Defaults to "udp".
+      ipv6 (optional): Whether the address is IPv6. Defaults to False.
+      workers (optional): The number of server processes. Defaults to 3.
+      system_samples (optional): A boolean value that indicates whether system_samples
+        are produced that report the throughput of this component. Defaults to False.
+
+    Exceptions:
+      socket.error:
+        If attempting to bind to an invalid address.
+      socket.gaierror:
+        If attempting to get address info from an invalid host or port.
+      ValueError:
+        If a non-integer port is given.
+    """
+    self.host = host
+    self.port = int(port)
+    self.sample_type = str(sample_type)
+    self.ipv6 = bool(ipv6)
+    self.workers = int(workers)
+    self.system_samples = bool(system_samples)
+
+    # System Samples Setup
+    self.sensed_data = 0
+    self.system_samples_updated = time.time()
+    self.system_samples_period = 1.0
+
+    # Socket Setup
+    if not self.ipv6:
+      addrinfo = socket.getaddrinfo(self.host, self.port, socket.AF_INET,
+                                    socket.SOCK_STREAM)
+      self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      address = addrinfo[0][4]
+    else:
+      addrinfo = socket.getaddrinfo(self.host, self.port, socket.AF_INET6,
+                                    socket.SOCK_STREAM)
+      self.socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+      address = addrinfo[0][4]
+    self.socket.settimeout(1.0)
+    try: self.socket.bind(address)
+    except socket.error as e:
+      self.socket.close()
+      raise e
+    self.max_buffer_size = 4096
+
+    # Address Lookup Setup
+    # Address lookup is by the tuple (patient_id, device_id) -> IP address.
+    # TODO: Under development! May be used in the future for PIMAP commands.
+    self.addresses_by_id = {}
+
+    # Multiprocess Setup
+    self.running = multiprocessing.Value(ctypes.c_bool, True)
+    self.pimap_data_queue = multiprocessing.Queue()
+    self.workers = self.workers
+    self.worker_processes = []
+    for i in range(self.workers):
+      worker_process = multiprocessing.Process(target=self._sense_worker, daemon=False)
+      self.worker_processes.append(worker_process)
+      worker_process.start()
+
+  def _sense_worker(self):
+    """Worker process
+
+    Used internally to create TCP server processes.
+    """
+    while self.running.value:
+      try:
+        self.socket.listen(self.workers)
+        (conn, address) = self.socket.accept()
+        with conn:
+          previous_data = ""
+          received_coded = conn.recv(self.max_buffer_size)
+          while received_coded:
+            received = received_coded.decode()
+            # If valid PIMAP sample/metric is received add it to the queue.
+            if pu.validate_datum(received):
+              received = previous_data + received
+              pimap_data_split = received.split(";;")
+              pimap_data = [inc_pimap + ";;" for inc_pimap in pimap_data_split[:-1]]
+              previous_data = pimap_data_split[-1]
+              for pimap_datum in pimap_data:
+                # Add lookup addresses of incoming PIMAP data.
+                patient_id = pu.get_patient_id(pimap_datum)
+                device_id = pu.get_device_id(pimap_datum)
+                self.pimap_data_queue.put(pimap_datum)
+            else:
+              patient_id = address[0]
+              device_id = address[1]
+              sample = received
+              pimap_datum = pu.create_pimap_sample(self.sample_type, patient_id,
+                                                   device_id, sample)
+              self.pimap_data_queue.put(pimap_datum)
+              # TODO: Under development! May be used in the future for PIMAP commands.
+            self.addresses_by_id[(patient_id, device_id)] = address
+            received_coded = conn.recv(self.max_buffer_size)
+      except socket.timeout: continue
+
+
+  def sense(self):
+    """Core interaction of PIMAP Sense TCP.
+
+    Returns:
+      A list of PIMAP samples/metrics sensed since last call to sense().
+    """
+    # Get all PIMAP data from the queue.
+    pimap_data = []
+    while not self.pimap_data_queue.empty():
+      pimap_data.append(self.pimap_data_queue.get())
+
+    # Sort the PIMAP data by timestamp. The PIMAP data can be out of order because we are
+    # using multiple processes to sense it.
+    pimap_data.sort(key=lambda x: pu.get_timestamp(x))
+
+    # Track the amount of sensed PIMAP data.
+    self.sensed_data += len(pimap_data)
+
+    # If system_samples is True and a system_sample was not created within the last
+    # system_samples period, create a system_sample.
+    pimap_system_samples = []
+    if (self.system_samples and
+        (time.time() - self.system_samples_updated > self.system_samples_period)):
+      sample_type = "system_samples"
+      # Identify PIMAP Sense using the host and port.
+      patient_id = "sense"
+      device_id = (self.host, self.port)
+      sensed_data_per_s = self.sensed_data/(time.time() - self.system_samples_updated)
+      sample = {"throughput":sensed_data_per_s}
+      system_sample = pu.create_pimap_sample(sample_type, patient_id, device_id, sample)
+      pimap_system_samples.append(system_sample)
+
+      # Reset system_samples variables.
+      self.system_samples_updated = time.time()
+      self.sensed_data = 0
+
+    return pimap_data + pimap_system_samples
+
+  def close(self):
+    """Safely stop the TCP server.
+
+    Terminates server processes and closes the socket.
+    """
+    self.running.value = False
+    for worker_process in self.worker_processes:
+      worker_process.join()
+    self.socket.close()
+
+# Deprecated Methods: May be used in the future.
+#
+#  # get_max_buffer_size: Get the max buffer size.
+#  def get_max_buffer_size(self):
+#    return self.max_buffer_size
+#
+#  # send_command: Sends the given command to the address based on the given patient_id
+#  # and device_id.
+#  # Under Development!
+#  def send_command(self, patient_id, device_id, command):
+#    """Under Development!
+#
+#    """
+#    if (str(patient_id), str(device_id)) in self.addresses_by_id:
+#        address = self.addresses_by_id[(str(patient_id), str(device_id))]
+#        self.socket.sendto(command.encode(), address)
---
+++ tests/testpimapsensetcp.py
@@ -0,0 +1,208 @@
+"""Unittest file for pimapsensetcp.py
+
+To run the tests in the top level directory type the command:
+PYTHONPATH=. python tests/testpimapsensetcp.py
+
+License:
+Author: Sam Mansfield
+"""
+import ast
+import socket
+import threading
+import time
+import unittest
+from pimap import pimapsensetcp as pset
+from pimap import pimaputilities as pu
+
+class PimapSenseTcpTestCase(unittest.TestCase):
+  def test_creating_and_closing_correct_usage(self):
+    # Test using default values.
+    sense = pset.PimapSenseTcp()
+    self.assertEqual(sense.socket.family, socket.AF_INET)
+    for worker in sense.worker_processes:
+      self.assertTrue(worker.is_alive())
+    sense.close()
+    for worker in sense.worker_processes:
+      self.assertFalse(worker.is_alive())
+
+    # Test using an IPv4 loopback address.
+    host = "localhost"
+    port = 1234
+    sense = pset.PimapSenseTcp(host, port)
+    self.assertEqual(sense.socket.family, socket.AF_INET)
+    for worker in sense.worker_processes:
+      self.assertTrue(worker.is_alive())
+    sense.close()
+    for worker in sense.worker_processes:
+      self.assertFalse(worker.is_alive())
+
+    # Test using an IPv4 non-loopback address.
+    host = socket.gethostname()
+    port = 1234
+    sense = pset.PimapSenseTcp(host, port)
+    self.assertEqual(sense.socket.family, socket.AF_INET)
+    for worker in sense.worker_processes:
+      self.assertTrue(worker.is_alive())
+    sense.close()
+    for worker in sense.worker_processes:
+      self.assertFalse(worker.is_alive())
+
+    # Test using an IPv6 loopback address.
+    host = "localhost"
+    port = 1234
+    ipv6 = True
+    sense = pset.PimapSenseTcp(host, port, ipv6=ipv6)
+    self.assertEqual(sense.socket.family, socket.AF_INET6)
+    for worker in sense.worker_processes:
+      self.assertTrue(worker.is_alive())
+    sense.close()
+    for worker in sense.worker_processes:
+      self.assertFalse(worker.is_alive())
+
+    # Test using an IPv6 non-loopback address.
+    host = socket.gethostname()
+    port = 1234
+    ipv6 = True
+    sense = pset.PimapSenseTcp(host, port, ipv6=ipv6)
+    self.assertEqual(sense.socket.family, socket.AF_INET6)
+    for worker in sense.worker_processes:
+      self.assertTrue(worker.is_alive())
+    sense.close()
+    for worker in sense.worker_processes:
+      self.assertFalse(worker.is_alive())
+
+    # Test with system_samples
+    sense = pset.PimapSenseTcp(system_samples=True)
+    time.sleep(sense.system_samples_period)
+    system_samples = sense.sense()
+    sense.close()
+    self.assertTrue(len(system_samples) > 0)
+    system_sample = system_samples[0]
+    sample = ast.literal_eval(pu.get_data(system_sample))
+    throughput = sample["throughput"]
+    correct_throughput = 0.0
+    self.assertEqual(throughput, correct_throughput)
+
+  def test_creating_and_closing_incorrect_usage(self):
+    # Test creating with an invalid host.
+    host = "google.com"
+    port = 1234
+    self.assertRaises(socket.error, pset.PimapSenseTcp, host, port)
+
+    # Test creating with an invalid nonsense host.
+    host = "nonsense"
+    port = 1234
+    self.assertRaises(socket.error, pset.PimapSenseTcp, host, port)
+
+    # Test creating with a valid host and an invalid port.
+    host = "localhost"
+    port = 1000000000
+    self.assertRaises(socket.gaierror, pset.PimapSenseTcp, host, port)
+
+    # Test creating with a non-integer port.
+    host = "localhost"
+    port = "nonsense"
+    self.assertRaises(ValueError, pset.PimapSenseTcp, host, port)
+
+  def test_sense(self):
+    # Test pimap data without system_samples.
+    sense = pset.PimapSenseTcp()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((sense.host, sense.port))
+    sample_type = "test_type" 
+    patient_id = "test"
+    device_id = "1"
+    sample = 0
+    sent_pimap_samples = []
+    stop_time = time.time() + sense.system_samples_period
+    while time.time() < stop_time:
+      pimap_sample = pu.create_pimap_sample(sample_type, patient_id, device_id, sample)
+      s.send(pimap_sample.encode())
+      time.sleep(0.001)
+      sent_pimap_samples.append(pimap_sample)
+      sample += 1
+    s.close()
+
+    sensed_pimap_samples = []
+    while len(sensed_pimap_samples) < len(sent_pimap_samples):
+      sensed_pimap_samples.extend(sense.sense())
+    sense.close()
+
+    self.assertEqual(len(sent_pimap_samples), len(sensed_pimap_samples))
+    for sent_psample, sensed_psample in zip(sent_pimap_samples, sensed_pimap_samples):
+      self.assertEqual(sent_psample, sensed_psample)
+
+    # Test generic data without system_samples.
+    sense = pset.PimapSenseTcp()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    shost = "127.0.0.1"
+    sport = 3500
+    addr_info =  socket.getaddrinfo(shost, sport, socket.AF_INET, socket.SOCK_STREAM)
+    addr = addr_info[0][4]
+    s.bind(addr)
+    s.connect((sense.host, sense.port))
+    data = 0
+    sent_data = []
+    stop_time = time.time() + sense.system_samples_period
+    while time.time() < stop_time:
+      s.send(str(data).encode())
+      time.sleep(0.001)
+      sent_data.append(data)
+      data += 1
+    sock_name = s.getsockname()
+    s.close() 
+
+    sensed_pimap_samples = []
+    while len(sensed_pimap_samples) < len(sent_data):
+      sensed_pimap_samples.extend(sense.sense())
+    sense.close()
+
+    self.assertEqual(len(sent_data), len(sensed_pimap_samples))
+    for sent_data, sensed_psample in zip(sent_data, sensed_pimap_samples):
+      sample_type = pu.get_type(sensed_psample)
+      patient_id = pu.get_patient_id(sensed_psample)
+      device_id = pu.get_device_id(sensed_psample)
+      sensed_data = int(pu.get_sample(sensed_psample))
+      self.assertEqual(sample_type, sense.sample_type)
+      self.assertEqual(patient_id, shost)
+      self.assertEqual(device_id, str(sport))
+      self.assertEqual(sent_data, sensed_data)
+
+    # Test pimap data with system_samples.
+    sense = pset.PimapSenseTcp(system_samples=True)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((sense.host, sense.port))
+    sample_type = "test_type"
+    patient_id = "test"
+    device_id = "1"
+    sample = 0
+    sent_pimap_samples = []
+    stop_time = time.time() + sense.system_samples_period
+    while time.time() < stop_time:
+      pimap_sample = pu.create_pimap_sample(sample_type, patient_id, device_id, sample)
+      s.send(pimap_sample.encode())
+      sent_pimap_samples.append(pimap_sample)
+      sample += 1
+    s.close() 
+
+    sensed_samples = []
+    while len(sensed_samples) < len(sent_pimap_samples):
+      sensed_samples.extend(sense.sense())
+    sense.close()
+
+    sensed_system_samples = list(filter(lambda x: pu.get_type(x) == "system_samples",
+                                        sensed_samples))
+    sensed_pimap_samples = list(filter(lambda x: pu.get_type(x) == "test_type",
+                                       sensed_samples))
+    self.assertEqual(len(sent_pimap_samples), len(sensed_pimap_samples))
+    for sent_psample, sensed_psample in zip(sent_pimap_samples, sensed_pimap_samples):
+      self.assertEqual(sent_psample, sensed_psample)
+
+    self.assertTrue(len(sensed_system_samples) > 0)
+    system_sample = sensed_system_samples[0]
+    sample = ast.literal_eval(pu.get_data(system_sample))
+    throughput = sample["throughput"]
+    self.assertTrue(throughput > 0.0)
+
+if __name__ == "__main__":
+  unittest.main()
